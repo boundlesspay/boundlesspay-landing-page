d9879fe5a7561d9e6e0b99c9fc725cbd247d65cd
freddance <70547931+freddance@users.noreply.github.com>
2022-10-25 18:39:57 +0100

    new update

--- pages/terms-of-use.vue
+++ pages/terms-of-use.vue
@@ -49,11 +49,168 @@
 
           <h3 style="color: #003399;" class="mt-4">Eligibilty</h3>
 
-          <div style="font-size: 36px; line-height: 67px;">‘Know Your Customer’ or ‘KYC’ also known as identification and verification procedures are required for all the Transactions
+          <div style="font-size: 35px; line-height: 63px;">‘Know Your Customer’ or ‘KYC’ also known as identification and verification procedures are required for all the Transactions
             on BoundlessPay.com. All new Users as well as existing users of the Platform must pass the verification procedure. BoundlessPay.com reserves the right to limit Users who have not passed verification to certain functionalities on the platform. If the User refuses to provide required documents
             and information under KYC, BoundlessPay.com reserves the right to terminate the provision of its services to the User.</div>
         </div>
-    </div>
+
+        <p class="mt-4">The User is solely responsible to provide BoundlessPay.com with relevant documents and personal information that contain accurate details about the user. A User might be considered fraudulent if such a user provides counterfeit documents and false personal information.
+
+         <p> The User hereby authorizes BoundlessPay.com to, directly or indirectly (through third party services), make any inquiries to double-check the relevance and accuracy of the information provided by the user for the purpose of verification. Security measures will be put in place to protect users’ personal data from destruction, alteration or sale.</p>
+
+          Note that we may process your personal data for only the purpose of providing the services which you have asked us to as well as to comply with regulatory requirements.
+
+
+
+        <h3 style="color: #003399;" class="mt-4">Account Security</h3>
+
+        <p>BoundlessPay is committed to providing its users with tips and measures that will help the user detect and prevent fraudulent activity which might lead to breach of account. But the User must note that he/she is responsible for maintaining the confidentiality of their Account’s credentials, including, (but not limited to) passwords, email, wallet address, balance and of all activity including transactions made through the account and avoid disclosure to third parties.
+
+        <p>You hereby acknowledge that BoundlessPay will not be responsible for the loss of your account as a result of unauthorized or careless use of the credentials you use to access our platform. Below are some of the security measures to take note of;</p>
+
+          No BoundlessPay.com personnel will never ask the User to disclose their password or personal credentials via any means of communication other than that of BoundlessPay.com platform. Any message the User receives other than from our site should be reported to us via fraudalert@boundlesspay.com as soon as possible.
+
+          <p>The user might consider changing his/her password at an interval of every three (3) months to reduce the risk of a security breach on the user’s account. BoundlessPay also advises the user should not choose a password that can be easily guessed from the user’s personal information such as name, age, date of birth. The User is also advised to never allow anyone to access his/her Account on his/her behalf.
+          </p>
+
+          If the User has any security concerns about his/her Account as to whether the login details has been lost, misappropriated or compromised, the User is advised to change the password immediately or contact support as soon as possible at support@boundlesspay.com
+
+          <p>Users are advised to always ensure that their login details are not stored or recorded by the browser.
+          </p>
+
+          Two-factor authentication should be enabled to ensure the security of users’ accounts.
+
+
+        <h3 style="color: #003399;" class="mt-4">Responsibility</h3>
+
+        <p>BoundlessPay.com assumes and takes all responsibility for all aspects that pertain to its website, customer service, support and resolution of disputes.</p>
+
+        <h3 style="color: #003399;" class="mt-4">Payment Methods</h3>
+
+        <p>The pre-installed BoundlessPay digital wallet can be funded by direct crypto deposits. Its debit card can also be funded directly with cryptocurrencies.</p>
+
+        <h3 style="color: #003399;" class="mt-4">Payment Methods</h3>
+
+        <p>It is important for users to note that there are no fixed rates on the BoundlessPay platform. The rates for various transactions in crypto or fiat are constantly updated and it is best for users to visit the mobile application or the website before making any transactions. The user must acknowledge the following;</p>
+
+        <p>– &nbsp;&nbsp;         Every Cryptocurrency’s network can deduct its own network fee.</p>
+        <p>– &nbsp; &nbsp;  BoundlessPay reserves the right to change the Transaction fee and rates from time to time.</p>
+        <p>– &nbsp;&nbsp;     Fees and rates are charged depending on the kind of transaction being made by the user on the platform.</p>
+
+        <h3 style="color: #003399;" class="mt-4">Customer Privacy Policy</h3>
+        <p>BoundlessPay will take every necessary measures to protect and safeguard users’ personal information in accordance with data protection regulations and as outlined in our privacy policy which is available at www.boundlesspay.com/privacy-policy .</p>
+
+
+        <h3 style="color: #003399;" class="mt-4">Notice and Communications</h3>
+        <p>BoundlessPay reserves the right to send notices to and communicate with the User from time to time by any means of communication available to BoundlessPay.com depending on the contact details provided by the User.
+
+          The user might agree to receive these notices in electronic form and be bound by them, if the Terms of Use requires users to do so.
+
+          The main official information channels of BoundlessPay is by email, notification from the mobile application and blog.</p>
+
+
+   <h3 style="color: #003399;" class="mt-4">Availability of Services</h3>
+        <p>BoundlessPay will work tirelessly to keep the Site up and running at all times. However, all online services suffer from occasional disruptions and outages and BoundlessPay is not liable for any disruption or loss the User may suffer as a result. Thus, BoundlessPay does not provide any guarantees that access to the Site will not be interrupted or that there will be no delays, failures, errors, omissions of any kind in the provision of its services.</p>
+
+      <p>BoundlessPay will see to it that the disruptions are reduced to the barest minimum. BoundlessPay will ensure that its users can normally access the Site in accordance with the Terms of Use. BoundlessPay may suspend the platform for maintenance and will make reasonable efforts to give the user a prior notice but the user must acknowledge that this may not be possible in a situation of emergency.</p>
+
+        <h3 style="color: #003399;" class="mt-4">Illegal Activities</h3>
+
+        <p>
+          BoundlessPay reserves the right to terminate your account at any point in time if proven beyond reasonable doubt that your account is being used for illicit purposes in order to comply with regulatory requirements issued by law enforcement agencies for the prevention of financial crime.
+
+        <p>   Users will acknowledge that they will not use the BoundlessPay account for any illegal purposes as BoundlessPay will not hesitate to report any suspicious activity to the relevant law enforcement agencies.</p>
+
+        Users acknowledge the account will not be used for anything related to the following;
+
+        <li>Violation of these terms of use or any agreement with BoundlessPay.</li>
+        <li>Infringement or violation of certain rights such as copyrights, industrial property rights of BoundlessPay or rights of any person or entity.</li>
+        <li>Promotion of human trafficking activities.</li>
+        <li>Money laundering and terrorist financing.</li>
+        <li>Peddling hard drugs and narcotics.</li>
+        <li>Involvement with illegal goods, services or transactions..</li>
+        <li>Involvement and facilitation of fraudulent activities;</li>
+        <li> Provision of fake or counterfeit identification documents</li>
+        <li> Violation of applicable laws</li>
+
+        <h3 style="color: #003399;" class="mt-4">No Financial Advice</h3>
+
+        <p>
+          BoundlessPay.com does not provide any financial, investment or legal advice in connection with the Services provided by our platform. BoundlessPay.com may provide information on the price changes or movements of Cryptocurrencies and events that have affected the price of Cryptocurrencies via our mobile application or social media platforms but such do not constitute and should not be considered as an investment or financial advice.
+
+          <p>Also note that the contents displayed on our platforms are mainly for marketing and informational purposes and should not be deemed as financial advice. Any decision to buy or sell Cryptocurrencies is strictly based on user’s discretion and decision and BoundlessPay will not be responsible for any loss incurred in the process of doing such.</p>
+
+        <h3 style="color: #003399;" class="mt-4">Modification to Terms of Use</h3>
+
+        <p>BoundlessPay reserves the right to make changes to these terms and conditions at any point in time with or without prior notice. It is in best interest to visit the website from time to time to stay updated with the updated terms of use as your continued use of the BoundlessPay platform will be considered as acknowledgement and agreement to the terms.</p>
+
+       <p> However, we will do our best to notify you of any modifications made to the terms of use which might affect you by asking you to confirm your acceptance to determine the continuation of providing you with our services. In cases where the user does not agree with any such modification, the User’s sole and exclusive remedy is to terminate the use of the Services and close the Account.</p>
+
+    <h3 style="color: #003399;" class="mt-4">Termination of Terms of Use & Restriction</h3>
+
+        <p> The User may terminate the Terms of Use and close their Account at any time, following settlement of any pending transactions on the BoundlessPay platform.</p>
+          <p> The User must also acknowledge that BoundlessPay may, by giving notice or at its sole discretion terminate his/her access to the site and restrict to his/her Account. This BoundlessPay’s right to: restrict, suspend or discontinue its services to users’ Accounts, prohibit access to the Site and its content, services, and as well take legal steps to keep the users off the platform if we think that they are creating technical problems or possible legal liabilities or infringing on the intellectual property rights of third parties or being inconsistent with the this Terms of Use.
+          </p>
+          <p> In addition, we may at our discretion, suspend or terminate Accounts of Users for any of the following without limitation:
+          </p>
+
+
+
+        <p>– &nbsp;&nbsp;   Violations of these Terms of Use</p>
+        <p>– &nbsp; &nbsp;   Attempts to gain unauthorized access to our platform or another User’s account.</p>
+        <p>– &nbsp; &nbsp;    Fraudulent payment for Transactions.</p>
+        <p>– &nbsp; &nbsp;   Usage of the Service to perform illegal activities such as money laundering, financing terrorism, or other criminal activities.</p>
+        <p>– &nbsp; &nbsp;   For regulatory compliance reasons such as at the request of law enforcement agencies.</p>
+        <p>– &nbsp; &nbsp;   Emergency technical or operational difficulties</p>
+        <p>– &nbsp; &nbsp;    BoundlessPay also reserves the right to close down unconfirmed Accounts or Accounts that have been dormant for a period of six (6) months or more. The User agrees that BoundlessPay will not be responsible to them or to any third party company for termination of their Account or discontinued access to our Site.</p>
+
+
+        <h3 style="color: #003399;" class="mt-4">Termination of Terms of Use & Restriction</h3>
+
+        <div style="font-size: 24px;
+line-height: 40px;">
+          IN NO CASE SHALL BOUNDLESSPAY, ITS AFFILIATES, PARENT COMPANIES, OFFICERS, DIRECTORS, EMPLOYEES, AGENTS, MERCHANTS AND ALL THIRD PARTY SERVICE PROVIDERS AND RELATED ENTITIES BE LIABLE TO THE USER OR ANY OTHER PERSON FOR ANY DIRECT, INDIRECT, INCIDENTAL, DAMAGES WHATSOEVER, INCLUDING ANY THAT MAY RESULT FROM INTERACTION OR INABILITY TO USE THE BOUNDLESS PLATFORM.
+
+          USERS MUST NOTE THAT IN NO CASE WILL BOUNDLESSPAY WILL LIABLE FOR ANY INTERRUPTION OF SERVICES THAT ARE LINKED TO THIS SITE.
+
+          LIKEWISE, BOUNDLESSPAY WILL NOT BE LIABLE FOR LOSS OF ANY KIND AS A RESULT OF USERS’ USE OF THIS SITE OR THE SERVICES FOUND AT THIS SITE NOR ANY OF BOUNDLESSPAY’S CHANNELS OF COMMUNICATION INCLUDING SOCIAL MEDIA PLATFORMS AND WHETHER OR NOT CEX.IO IS ADVISED OF THE POSSIBILITY OF THE OCCURRENCE OF SUCH DAMAGES.
+
+          IN ADDITION, THE USER SPECIFICALLY ACKNOWLEDGE AND AGREE THAT IN NO CASE SHALL BOUNDLESSPAY’S TOTAL LIABILITY EXCEED THE TOTAL AMOUNT PAID BY THE USER FOR A PARTICULAR SERVICE WITH REGARDS TO THE SUCH SERVICE.
+
+          USERS WILL ACKNOWLEDGE THAT THIS LIMITATION OF LIABILITY SHALL APPLY TO THE FULLEST EXTENT PERMITTED BY REQUIRED REGULATION AND SHALL SURVIVE ANY TERMINATION OF THIS AGREEMENT OR THE USER’S USE OF THIS SITE OR THE SERVICES FOUND AT BOUNDLESSPAY’S WEBSITE.
+        </div>
+
+        <h3 style="color: #003399;" class="mt-4">Agreement</h3>
+        <p>You hereby acknowledge that you have read this terms of use, understood and you agree to the terms, privacy policy and other contained therein
+               by reference by accessing the BoundlessPay platform or using any of its services. Note that his
+          Agreement entered into by the User and BoundlessPay.com, supersedes all prior discussions, agreements, or correspondence of any kind. </p>
+
+ <h3 style="color: #003399;" class="mt-4">Contact Us</h3>
+        <p>If you have any questions relating to these Terms of Use, your rights and feedbacks arising from
+          these Terms and/or your use of the Site please, contact us via support@boundlesspay.com</p>
+
+
+
+        –
+
+        –
+
+        –
+
+        –
+
+        –
+
+        –
+
+
+
+
+
+
+
+      </div>
+
 
 
 
